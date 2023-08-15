c04b20a7c1816507842e0751bf9c8cf118e1260c
Selim Arda Cevik <sardacevik@icloud.com>
2023-08-15 23:03:50 +0300

    inital commit

---
+++ src/main/java/com/selimlendiniz/security/auth/AuthenticationService.java
@@ -0,0 +1,63 @@
+package com.selimlendiniz.security.auth;
+
+import com.selimlendiniz.security.config.JwtService;
+import com.selimlendiniz.security.user.Roles;
+import com.selimlendiniz.security.user.User;
+import com.selimlendiniz.security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Roles.USER)
+                .build();
+
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ src/main/java/com/selimlendiniz/security/user/Roles.java
@@ -0,0 +1,6 @@
+package com.selimlendiniz.security.user;
+
+public enum Roles {
+    USER,
+    ADMIN
+}
